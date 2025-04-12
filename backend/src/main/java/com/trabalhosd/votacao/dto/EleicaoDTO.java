5941ed9f665f63f954a21cf2dc188e0b2c7eee44
Yan <yanvzp@gmail.com>
2025-04-12 14:52:27 -0300

    feat: removing unused import

--- backend/src/main/java/com/trabalhosd/votacao/dto/EleicaoDTO.java
+++ backend/src/main/java/com/trabalhosd/votacao/dto/EleicaoDTO.java
@@ -1,16 +1,10 @@
 package com.trabalhosd.votacao.dto;
 
-import com.trabalhosd.votacao.entity.Candidato;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
