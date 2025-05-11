254215689e37ca563e3ce87ca95d96c9e9ade535
Yan Guilherme Viana Leite <yguilherme@ciandt.com>
2025-05-11 17:09:31 -0300

    Mudancas

--- sistema-votacao-frontend/src/app/app.routes.ts
+++ sistema-votacao-frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { VotacaoComponent } from './pages/votacao/votacao.component';
+import { BemvindoComponent } from './pages/bemvindo/bemvindo.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'inicio', pathMatch: 'full' }, // Redirecionar vazio para "inicio"
+  { path: '', redirectTo: 'bemvindo', pathMatch: 'full' }, // Redirecionar vazio para "inicio"
 
+  { path: 'bemvindo', component: BemvindoComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'votacao/:id', component: VotacaoComponent },
+  { path: 'votacao', component: VotacaoComponent },
 ];
