d6588e89d1c9572b2faa9098485ff154c45e4111
Yan <yanvzp@gmail.com>
2025-06-13 23:00:19 -0300

    mudancas nos ips de toda a comunicacao do sistema

--- frontend/src/app/service/api.ts
+++ frontend/src/app/service/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const apiCore = axios.create({
-  baseURL: 'http://localhost:8081',
+  baseURL: 'http://192.168.3.4:8081',
 });
 
 export const apiBase = axios.create({
-  baseURL: 'http://coletor.local:8080', // por enquanto, fazer o front trocar
+  baseURL: 'http://192.168.3.4:8080', // por enquanto, fazer o front trocar
 });
